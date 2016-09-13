aa29f654bc9d13b0c63694d2f02d49a3c410ade7
shriyaEsha <shriya.sasank@gmail.com>
2016-09-13 06:00:45 +0530

    Added portfolio-desc,email,page trans still rem

--- appjs/controller.js
+++ appjs/controller.js
@@ -25,70 +25,70 @@ $scope.nav = function(clicked)
 
 /*EVENTS*/
 myApp.controller('masonryController',['$scope','$http','$location','$timeout',function($scope,$http,$location,$timeout){
+
+$scope.pagetitle = $location.path().substr(1);
 $scope.bricks = [
-{src:'images/portraits/abs22.jpg'},
-{src:'images/nature/water.jpg'},
-{src:'images/nature/flowers.jpg'},
-{src:'images/nature/kitty.jpg'},
-{src:'images/comics/comic24.jpg'},
-{src:'images/comics/comic34.jpg'},
-{src:'images/comics/comic12.jpg'},
-{src:'images/comics/comic15.jpg'},
-{src:'images/comics/comic25.1.jpg'},
-{src:'images/comics/comic28.jpg'},
+{src:'images/portraits/abs22.jpg',id:1},
+{src:'images/nature/water.jpg',id:2},
+{src:'images/nature/flowers.jpg',id:3},
+{src:'images/nature/kitty.jpg',id:4},
+{src:'images/comics/comic24.jpg',id:5},
+{src:'images/comics/comic34.jpg',id:6},
+{src:'images/comics/comic12.jpg',id:7},
+{src:'images/comics/comic15.jpg',id:8},
+{src:'images/comics/comic25.1.jpg',id:9},
+{src:'images/comics/comic28.jpg',id:10},
 ];
 
 $scope.pbricks = [
-{src:'images/portraits/abs22.jpg'},
-{src:'images/nature/water.jpg'},
-{src:'images/nature/flowers.jpg'},
-{src:'images/nature/kitty.jpg'},
-{src:'images/comics/comic24.jpg'},
-{src:'images/comics/comic34.jpg'},
-{src:'images/comics/comic12.jpg'},
-{src:'images/comics/comic15.jpg'},
-{src:'images/comics/comic25.1.jpg'},
-{src:'images/comics/comic28.jpg'},
+{src:'images/portraits/abs22.jpg',id:1},
+{src:'images/portraits/jin22.jpg',id:2},
+{src:'images/portraits/nivi22.jpg',id:3},
+{src:'images/portraits/feez 21st.jpg',id:4},
+{src:'images/portraits/pujo.jpg',id:5},
+{src:'images/portraits/pink mom.jpg',id:6},
+{src:'images/portraits/anusha.jpg',id:7},
+{src:'images/portraits/anu mami.jpg',id:8},
+
 ];
 
 $scope.nbricks = [
-{src:'images/portraits/abs22.jpg'},
-{src:'images/nature/water.jpg'},
-{src:'images/nature/flowers.jpg'},
-{src:'images/nature/kitty.jpg'},
-{src:'images/comics/comic24.jpg'},
-{src:'images/comics/comic34.jpg'},
-{src:'images/comics/comic12.jpg'},
-{src:'images/comics/comic15.jpg'},
-{src:'images/comics/comic25.1.jpg'},
-{src:'images/comics/comic28.jpg'},
+{src:'images/nature/flowers.jpg',id:1},
+{src:'images/nature/kitty.jpg',id:2},
+{src:'images/nature/banana.jpg',id:3},
 ];
 $scope.sbricks = [
-{src:'images/portraits/abs22.jpg'},
-{src:'images/nature/water.jpg'},
-{src:'images/nature/flowers.jpg'},
-{src:'images/nature/kitty.jpg'},
-{src:'images/comics/comic24.jpg'},
-{src:'images/comics/comic34.jpg'},
-{src:'images/comics/comic12.jpg'},
-{src:'images/comics/comic15.jpg'},
-{src:'images/comics/comic25.1.jpg'},
-{src:'images/comics/comic28.jpg'},
+{src:'images/nature/dahlias.jpg',id:1},
+{src:'images/comics/comic24.jpg',id:2},
+{src:'images/nature/scenery.jpg',id:3},
+{src:'images/nature/desert.jpg',id:4},
+{src:'images/nature/water.jpg',id:5},
 ];
 $scope.cbricks = [
-{src:'images/portraits/abs22.jpg'},
-{src:'images/nature/water.jpg'},
-{src:'images/nature/flowers.jpg'},
-{src:'images/nature/kitty.jpg'},
-{src:'images/comics/comic24.jpg'},
-{src:'images/comics/comic34.jpg'},
-{src:'images/comics/comic12.jpg'},
-{src:'images/comics/comic15.jpg'},
-{src:'images/comics/comic25.1.jpg'},
-{src:'images/comics/comic28.jpg'},
+{src:'images/comics/comic34.jpg',id:1},
+{src:'images/comics/comic6.jpg',id:2},
+{src:'images/comics/comic8.jpg',id:3},
+{src:'images/comics/comic12.jpg',id:4},
+{src:'images/comics/comic13-1.jpg',id:5},
+{src:'images/comics/comic15.jpg',id:6},
+{src:'images/comics/comic9.jpg',id:7},
+{src:'images/comics/comic25.1.jpg',id:8},
+{src:'images/comics/comic28.jpg',id:9},
+{src:'images/comics/comic26.jpg',id:10},
 ];
+$scope.img_id = "";
+$scope.category = "";
+$scope.show_image = function(img_src,img_id){
+$(".main").animate({'opacity':"0.1"});
+$(".image-viewer").addClass("showme").append("<img class='col-md-6 view-img' src='"+img_src+"'/>");
+$(".image-viewer").append("<div class='desc col-md-6 '>Desc</div>");
+};
+$scope.close = function(){
+	$(".main").animate({'opacity':1},100);
+	$(".image-viewer").removeClass("showme",100);
+	$(".image-viewer").find(".view-img").remove();
+	$(".image-viewer").find(".desc").remove();
 
-
-
+};
 
 }]);
--- appjs/directives.js
+++ appjs/directives.js
@@ -26,35 +26,16 @@ myApp.directive('compile', ['$compile', function ($compile) {
 				};
 
 }]);
-myApp.directive('tabsAnim', function() {
+myApp.directive('brickAnim', function() {
         console.log("tabsAnim");
         return{
         scope:{},
         restrict: 'AEC',
         replace:'true',
         link: function(scope,elem,attrs,$location){
-$(document).ready(function(){
-$(".close").click(function(){
-  closeall();
-});
-function closeall(){
-    $(".imagebox").each(function(){
-    
-  $(".navbar").show();
-  $(".category").show();
-      var elem = $(this);
-      setTimeout(function(){
-        $(elem).animate({'opacity':"1",'margin-left':"0px"},100);
-      },i*50+50);
-    });
-    $(".left").animate({'marginLeft':"-100%"},300,'easeInOutQuad');
-}
-
-$(".navbar-toggle").click(function(){
-  closeall();
-  
-});
-});
+          $(".masonry-brick").find("img").click(function(){
+          alert("hey");
+          });
            }
         };
     });
